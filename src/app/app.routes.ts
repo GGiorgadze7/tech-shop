10562d03986a9615d89e1f923f0a6b27fc931791
giorgi7 <173785707+GGiorgadze7@users.noreply.github.com>
2026-04-27 21:18:34 +0400

    added folders, burger menu, navigation and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Page404 } from './pages/page-404/page-404';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'', component:Home, title:'Tech Shop'},
+    {path:'about', component:About, title:'About'},
+    {path:'contact', component:Contact, title:'contact'},
+    {path:'**', component:Page404, title:"You Have Lost"}
+];
