be6d557e2ffdf509a1dfe19c96e9aff3852c2c56
giorgi7 <173785707+GGiorgadze7@users.noreply.github.com>
2026-05-16 20:19:03 +0400

    changed title and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,47 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { About } from './pages/about/about';
-import { Contact } from './pages/contact/contact';
 import { Page404 } from './pages/page-404/page-404';
 import { guestGuard } from './services/guards/guest-guard';
 import { authGuard } from './services/guards/auth-guard';
-import { cartGuard } from './services/guards/cart-guard';
-import { Passrecover } from './pages/auth/passrecover/passrecover';
 
 export const routes: Routes = [
   { path: '', component: Home, title: 'Tech Shop' },
-  { path: 'about', component: About, title: 'About' },
-  { path: 'contact', component: Contact, title: 'contact' },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+    title: 'About',
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact').then((m) => m.Contact),
+    title: 'contact',
+  },
 
   {
     path: 'user',
     loadComponent: () => import('./pages/auth/user/user').then((m) => m.User),
+    title: 'user',
     canActivate: [authGuard],
   },
 
   {
     path: 'signin',
     loadComponent: () => import('./pages/auth/signin/signin').then((m) => m.Signin),
+    title: 'signin',
     canActivate: [guestGuard],
   },
 
   {
     path: 'signup',
     loadComponent: () => import('./pages/auth/signup/signup').then((m) => m.Signup),
+    title: 'signup',
     canActivate: [guestGuard],
   },
 
   {
     path: 'cart',
     loadComponent: () => import('./pages/cart/cart').then((m) => m.Cart),
+    title: 'cart',
   },
 
   {
@@ -46,12 +54,14 @@ export const routes: Routes = [
   {
     path: 'passrecover',
     loadComponent: () => import('./pages/auth/passrecover/passrecover').then((m) => m.Passrecover),
+    title: 'emailrecovery',
   },
 
   {
     path: 'passrecover2',
     loadComponent: () =>
       import('./pages/auth/passrecover2/passrecover2').then((m) => m.Passrecover2),
+    title: 'passrecovery',
   },
 
   { path: '**', component: Page404, title: 'You Have Lost' },
